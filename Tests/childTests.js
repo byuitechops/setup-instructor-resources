4a863950e79838c064905e330bf028897be0a009
Zachary  Williams <wil11063@byui.edu>
2018-03-27 10:11:42 -0600

    Update childTests.js

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,32 +1,49 @@
-/*eslint-env node, es6*/
+// /*eslint-env node, es6*/
+
+// /* Dependencies */
+// const tap = require('tap');
+
+// function g1Tests(course, callback) {
+//     var srArr = course.info.standardResourcesArr;
+//     var hArr = course.info.headerArr;
+
+//     tap.equal(srArr.length, 6);
+
+//     tap.equal(srArr[0], 'Setup for Course Instructor');
+//     tap.equal(srArr[1], 'General Lesson Notes');
+//     tap.equal(srArr[2], 'Release Notes');
+//     tap.equal(srArr[3], 'Course Map');
+//     tap.equal(srArr[4], 'Teaching Group Directory');
+//     tap.equal(srArr[5], 'Online Instructor Community');
+
+//     tap.equal(hArr, 2);
+
+//     tap.equal(hArr[0], 'Standard Resources');
+//     tap.equal(hArr[1], 'Supplemental Resources');
+
+//     callback(null, course);
+// }
+
+// module.exports = [
+//         {
+//             gauntlet: 1,
+//             tests: g1Tests
+//         }
+// ];
 
 /* Dependencies */
 const tap = require('tap');
+const canvas = require('canvas-wrapper');
 
-function g1Tests(course, callback) {
-    var srArr = course.info.standardResourcesArr;
-    var hArr = course.info.headerArr;
+module.exports = (course, callback) => {
+    tap.test('child-template', (test) => {
 
-    tap.equal(srArr.length, 6);
+        test.pass('potato');
+        test.pass('tomato');
+        test.fail('avacado');
 
-    tap.equal(srArr[0], 'Setup for Course Instructor');
-    tap.equal(srArr[1], 'General Lesson Notes');
-    tap.equal(srArr[2], 'Release Notes');
-    tap.equal(srArr[3], 'Course Map');
-    tap.equal(srArr[4], 'Teaching Group Directory');
-    tap.equal(srArr[5], 'Online Instructor Community');
-
-    tap.equal(hArr, 2);
-
-    tap.equal(hArr[0], 'Standard Resources');
-    tap.equal(hArr[1], 'Supplemental Resources');
+        test.end();
+    });
 
     callback(null, course);
-}
-
-module.exports = [
-        {
-            gauntlet: 1,
-            tests: g1Tests
-        }
-];
+};
